6b95a8846e1b017aaf86fe08cd03c16ae5ded89b
Condere Jerome <jerome.condere@gmail.com>
2016-02-10 00:07:07 +0100

    continuing the unit tests

--- src/UnitTest/MailboxTest.java
+++ src/UnitTest/MailboxTest.java
@@ -11,6 +11,39 @@ import utils.box.Mailbox;
 
 public class MailboxTest {
 
+	@Test
+	public void TestaddInbox()
+	{
+		Mailbox alanMailbox=new Mailbox("Albox");
+		Inbox inAlbox=new Inbox("inAlbox", "mailbox");
+		alanMailbox.getInboxes().add(inAlbox);
+
+		if(alanMailbox.getInboxes().isEmpty())
+		{
+			fail("error the mailbox is empty");
+		}
+		String inboxName=alanMailbox.getInboxes().get(0).getName();
+		String mailboxName=alanMailbox.getInboxes().get(0).getMailboxName();
+		assertEquals("error on Inbox name",inboxName,"inAlbox");
+		assertEquals("error on mailbox name of inbox",mailboxName,"mailbox");
+	}
+	@Test
+	public void TestaddOutbox()
+	{
+		Mailbox alanMailbox=new Mailbox("Albox");
+		Outbox outAlbox=new Outbox("outAlbox", "mailbox");
+		alanMailbox.getOutboxes().add(outAlbox);
+
+		if(alanMailbox.getOutboxes().isEmpty())
+		{
+			fail("error the mailbox is empty");
+		}
+		String outboxName=alanMailbox.getOutboxes().get(0).getName();
+		String mailboxName=alanMailbox.getOutboxes().get(0).getMailboxName();
+		assertEquals("error on Outbox name",outboxName,"outAlbox");
+		assertEquals("error on mailbox name of Outbox",mailboxName,"mailbox");
+	}
+	
 	
 	@Test
 	public void TestaddInputConnection()
@@ -56,6 +89,8 @@ public class MailboxTest {
 		assertEquals("error on mailbox name for the inbox",mailboxName_of_outbox,"mailboxJen");
 	
 	}
+	
+	
 	
 	
 
